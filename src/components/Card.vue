bdec3cb8a3a80336712c6b2239558f8f180d62dc
yordisev <yordys12@gmail.com>
2021-09-05 14:18:08 -0500

    formatear numero

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -4,7 +4,7 @@
           <h5>{{pais.name}}</h5>
           <p class="card-text">
               <span class="badge badge-primary d-block mb-1">Nombre Nativo: {{pais.nativeName}}</span>
-              <span class="badge badge-success p-3 d-block mb-1">Poblacion: {{pais.population}}</span>
+              <span class="badge badge-success p-3 d-block mb-1">Poblacion: {{numeroFormato(pais.population)}}</span>
               <span class="badge badge-primary d-block mb-1">Capital: {{pais.capital}}</span>
               <span class="badge badge-primary d-block mb-1">Region: {{pais.region}}</span>
           </p>
@@ -14,7 +14,13 @@
 
 <script>
 export default {
-props:['pais']
+props:['pais'],
+setup(){
+    const numeroFormato = (numero) =>{
+        return new Intl.NumberFormat("de-DE").format(numero)
+    }
+    return {numeroFormato}
+}
 }
 </script>
 
